2080cd33b9ab39ce4f2378474891f21b2ad50cab
Fareeid <faridkim@gmail.com>
2023-09-06 15:12:25 +0300

    Initial setup of jaz_api_v03

---
+++ jaz_api_v03/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from .ping import ping  
+
+# xtype: ignore
+
+def create_application():
+    fastapi_app = FastAPI()
+    fastapi_app.include_router(ping.router)
+    return fastapi_app
+
+app = create_application()
